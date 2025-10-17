730620b7e9c2a135ac840b8cf4cd4880e432750c
LisNat <rivera.liseth@correounivalle.edu.co>
2025-10-17 18:35:53 -0500

    Adelanto de las primeras 6 funciones

--- src/main/scala/SubsecuenciaMasLarga/package.scala
+++ src/main/scala/SubsecuenciaMasLarga/package.scala
@@ -1,4 +1,59 @@
 package object SubsecuenciaMasLarga {
+  type Secuencia = Seq[Int]
+  type Subsecuencia = Seq[Int]
+
+  def subindices(i: Int, n: Int): Set[Seq[Int]] = {
+    if (i == n) Set(Seq())
+    else if (i > n) throw new Error("i debe ser menor o igual a n")
+    else {
+      val sinI = subindices(i + 1, n)
+      val conI = for (subsec <- sinI) yield i +: subsec
+      sinI ++ conI
+    }
+  }
+
+  def subSecuenciaAsoc(s: Secuencia, inds: Seq[Int]): Subsecuencia = {
+    for (i <- inds) yield s(i)
+  }
+
+  def subSecuenciasDe(s: Secuencia): Set[Subsecuencia] = {
+    for (inds <- subindices(0, s.length)) yield subSecuenciaAsoc(s, inds)
+  }
+
+  // Estrictamente creciente
+  def incremental(s: Subsecuencia): Boolean = {
+    (for {
+      i <- 0 until s.length - 1
+    } yield s(i) < s(i + 1)).forall(identity)
+  }
+
+  def subSecuenciasInc(s: Secuencia): Set[Subsecuencia] = {
+    for {
+      subsec <- subSecuenciasDe(s)
+      if incremental(subsec)
+    } yield subsec
+  }
+
+  
+  def subsecuenciaIncrementalMasLarga(s: Secuencia): Subsecuencia = {
+    if (s.isEmpty) Seq()
+    else {
+      val incrementales = subSecuenciasInc(s)
+      (for {
+        subsec <- incrementales
+      } yield subsec).maxBy(_.length)
+    }
+  }
+
+  def ssimlComenzandoEn(i: Int, s: Secuencia): Subsecuencia = {
+    // Devuelve la subsecuencia incremental mas larga de s que comienza en s(i)
+  ...
+  }
+
+  def subSecIncMasLargaV2(s: Secuencia): Subsecuencia = {
+    // Devuelve una subsecuencia incremental mas larga de s
+  ...
+  }
 
 
   
--- src/test/scala/pruebas.sc
+++ src/test/scala/pruebas.sc
@@ -1,2 +1,30 @@
 import SubsecuenciaMasLarga ._
 
+subindices(2,3)
+subindices(0,3)
+
+val s = Seq( 20, 30, 10, 40,  15, 16, 17)
+val s1= Seq( 20, 30, 10)
+val s2 = Seq(20, 30, 10, 40, 15, 16, 17, 11, 12, 13, 14)
+val s3 = Seq()
+
+subSecuenciaAsoc(s, Seq())
+subSecuenciaAsoc(s, Seq(0,2,4))
+subSecuenciaAsoc(s, Seq(1,2,4,6))
+
+subSecuenciasDe(s1)
+
+incremental(Seq(10, 15, 16, 17))
+incremental(Seq(30, 10, 40))
+incremental(Seq(20, 10))
+incremental(Seq(20,20))
+incremental(Seq(10))
+incremental(Seq())
+
+subSecuenciasInc(s)
+subSecuenciasInc(s1)
+
+subsecuenciaIncrementalMasLarga(s)
+subsecuenciaIncrementalMasLarga(s1)
+subsecuenciaIncrementalMasLarga(s2)
+subsecuenciaIncrementalMasLarga(s3)
